b599cf9c45ade40ca0c8cac26abf0ff9a8b7d24a
aokumasan <mk.alice02@gmail.com>
2019-12-01 18:07:24 +0900

    Implement empty cloud provider

--- pkg/cloudprovider/providers/nifcloud/nifcloud.go
+++ pkg/cloudprovider/providers/nifcloud/nifcloud.go
@@ -1,4 +1,65 @@
 package nifcloud
 
+import (
+	"io"
+
+	cloudprovider "k8s.io/cloud-provider"
+)
+
 // ProviderName is the name of this cloud provider
 const ProviderName = "nifcloud"
+
+// Cloud is an implementation of Interface, LoadBalancer and Instances for NIFCLOUD
+type Cloud struct {
+}
+
+func init() {
+	cloudprovider.RegisterCloudProvider(ProviderName, func(config io.Reader) (cloudprovider.Interface, error) {
+		return newNIFCLOUD()
+	})
+}
+
+func newNIFCLOUD() (cloudprovider.Interface, error) {
+	return &Cloud{}, nil
+}
+
+// Initialize provides the cloud with a kubernetes client builder and may spawn goroutines
+// to perform housekeeping or run custom controllers specific to the cloud provider.
+// Any tasks started here should be cleaned up when the stop channel closes.
+func (c *Cloud) Initialize(clientBuilder cloudprovider.ControllerClientBuilder, stop <-chan struct{}) {
+}
+
+// LoadBalancer returns a balancer interface. Also returns true if the interface is supported, false otherwise.
+func (c *Cloud) LoadBalancer() (cloudprovider.LoadBalancer, bool) {
+	return nil, false
+}
+
+// Instances returns an instances interface. Also returns true if the interface is supported, false otherwise.
+func (c *Cloud) Instances() (cloudprovider.Instances, bool) {
+	return nil, false
+}
+
+// Zones returns a zones interface. Also returns true if the interface is supported, false otherwise.
+func (c *Cloud) Zones() (cloudprovider.Zones, bool) {
+	return nil, false
+}
+
+// Clusters returns a clusters interface.  Also returns true if the interface is supported, false otherwise.
+func (c *Cloud) Clusters() (cloudprovider.Clusters, bool) {
+	return nil, false
+}
+
+// Routes returns a routes interface along with whether the interface is supported.
+func (c *Cloud) Routes() (cloudprovider.Routes, bool) {
+	return nil, false
+}
+
+// ProviderName returns the cloud provider ID.
+func (c *Cloud) ProviderName() string {
+	return ProviderName
+}
+
+// HasClusterID returns true if a ClusterID is required and set
+func (c *Cloud) HasClusterID() bool {
+	return false
+}

c31ecd2a5c65e03e1524d7710ed2697a88b3679c
sirococoa <yo-nakano@fujitsu.com>
2024-05-31 11:55:09 +0900

    Add NodeAddresses test

---
+++ pkg/cloudprovider/providers/nifcloud/nifcloud_instances_test.go
@@ -0,0 +1,105 @@
+package nifcloud_test
+
+import (
+	"context"
+
+	"github.com/nifcloud/nifcloud-cloud-controller-manager/pkg/cloudprovider/providers/nifcloud"
+	"github.com/nifcloud/nifcloud-cloud-controller-manager/test/helper"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("NodeAddresses", func() {
+	var ctrl *gomock.Controller
+	var region string = "east1"
+
+	BeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+	})
+
+	AfterEach(func() {
+		ctrl.Finish()
+	})
+
+	Context("single instance is existed", func() {
+		It("return the NodeAddress", func() {
+			ctx := context.Background()
+			testInstances := []nifcloud.Instance{*helper.NewTestInstance()}
+			nodeName := testInstances[0].InstanceID
+			expectedNodeAddress := []v1.NodeAddress{
+				{
+					Type:    v1.NodeExternalIP,
+					Address: testInstances[0].PublicIPAddress,
+				},
+				{
+					Type:    v1.NodeInternalIP,
+					Address: testInstances[0].PrivateIPAddress,
+				},
+			}
+
+			c := nifcloud.NewMockCloudAPIClient(ctrl)
+			c.EXPECT().
+				DescribeInstancesByInstanceID(gomock.Any(), []string{nodeName}).
+				Return(testInstances, nil).
+				Times(1)
+
+			cloud := &nifcloud.Cloud{}
+			cloud.SetClient(c)
+			cloud.SetRegion(region)
+
+			nodeAddress, err := cloud.NodeAddresses(ctx, types.NodeName(nodeName))
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(nodeAddress).Should(Equal(expectedNodeAddress))
+		})
+	})
+
+	Context("the instance is not existed", func() {
+		It("return error", func() {
+			ctx := context.Background()
+			testInstances := []nifcloud.Instance{}
+			nodeName := "testinstance"
+
+			notFoundErr := helper.NewMockAPIError(nifcloud.ExportErrorCodeInstanceNotFound)
+			c := nifcloud.NewMockCloudAPIClient(ctrl)
+			c.EXPECT().
+				DescribeInstancesByInstanceID(gomock.Any(), []string{nodeName}).
+				Return(testInstances, notFoundErr).
+				Times(1)
+
+			cloud := &nifcloud.Cloud{}
+			cloud.SetClient(c)
+			cloud.SetRegion(region)
+
+			nodeAddress, err := cloud.NodeAddresses(ctx, types.NodeName(nodeName))
+			Expect(err).Should(HaveOccurred())
+			Expect(nodeAddress).Should(BeNil())
+		})
+	})
+
+	Context("some instances have same InstanceID are existed", func() {
+		It("return error", func() {
+			ctx := context.Background()
+			testInstances := []nifcloud.Instance{*helper.NewTestInstance(), *helper.NewTestInstance()}
+			nodeName := testInstances[0].InstanceID
+
+			c := nifcloud.NewMockCloudAPIClient(ctrl)
+			c.EXPECT().
+				DescribeInstancesByInstanceID(gomock.Any(), []string{nodeName}).
+				Return(testInstances, nil).
+				Times(1)
+
+			cloud := &nifcloud.Cloud{}
+			cloud.SetClient(c)
+			cloud.SetRegion(region)
+
+			nodeAddress, err := cloud.NodeAddresses(ctx, types.NodeName(nodeName))
+			Expect(err).Should(HaveOccurred())
+			Expect(nodeAddress).Should(BeNil())
+		})
+	})
+})
